69d37be04fa69b9b05d1f169b31170621ceb3825
qhdrbs00 <152240814+qhdrbs00@users.noreply.github.com>
2023-12-05 17:13:06 +0900

    Add files via upload

--- tt.py
+++ tt.py
@@ -48,42 +48,52 @@ import matplotlib.pyplot as plt
 #         i
 col1, col2 = st.columns([1, 2])
 with col1:
-    st.image('토끼.jpg')
+    st.image('증명사진.jpg')
 with col2:
-    st.write('놓치면 후회할 인재(신수인👍, 시급 5만원, 대박 쩔어~~)')
-    '전화번호(☎) : 010-8773-4080'
-    '이메일(📧) : strustar@konuang.ac.kr'
-    '주소(🏠) : 충남 논산시 대학로121'
-col = st.columns(4)
+    st.write('놓치면 후회할 인재(서봉균👍)')
+    '전화번호(☎) : 010-7420-9328'
+    '이메일(📧) : sbk727@naver.com'
+    '주소(🏠) : 대전광역시 둔산남로 30'
+    '자격증(📜) : 운전면허 1종 보통, 컴퓨터 활용 능력 1급 , 위험물산업기사(예정), 태권도 1단'
+col = st.sidebar.columns(2)
 with col[0]:
-    st.link_button("구글🌐", "https://www.google.co.kr/?hl=ko")
+    st.link_button("인스타그램🌐", "https://instagram.com/tjqhdrbs?igshid=OGQ5ZDc2ODk2ZA==")
 with col[1]:
-    st.link_button("네이버🇳", "https://www.naver.com/")
-with col[2]:
-    st.link_button("다음", "https://www.daum.net/")
-with col[3]:
-    st.link_button("링크인", "https://kr.linkedin.com/")
+    st.link_button("페이스북", "https://www.facebook.com/profile.php?id=100005423596508&mibextid=ZbWKwL")
+''
+''
+'## :orange[자기 소개]'
+'안녕하세요 제 이름은 서봉균이고, 21살입니다. 끊임없이 도전하고 성장하는 것을 좋아하며, 새로운 경험을 통해 스스로 발전하는 것을 좋아하며, 새로운 경험을 통해 스스로 발전하는 것을 목표로 삼고 있습니다. 지금까지의 경험과 미래의 목표를 기반으로 소개합니다'
+'1.학력 및 전공'
+'건양대학교 재난안전소방학과에서 소방학을 전공 했습니다. 소방학을 전공하긴 했지만 재난과 안전도 주로 배웠기 때문에 여러가지 안전 상황에서 다양한 분석과 해결 능력을 갖춘 다양한 상황에서 활용될 수 있습니다. '
+'2.경력 및 프로젝트'
+'건양대학교 캡스톤 디자인을 통해 전기자동차 화재 시 탑습자의 인명 안전을 위한 사출 장치 라는 논문을 팀원과 작성하고 실제로 모형으로 제작하여 사출되는 현장을 구축했습니다. 팀 프로젝트 에서 팀원들과 원활한 소통과 협력을 통해 성공으로 프로젝트를 완료 했습니다.'
+'3.기술 및 능력'
+'위험물 산업 기사 자격증을 보유하고 있습니다. 이 자격증으로 인해 보다 안전한 산업 현장을 구축 할 수 있고 컴퓨터 활용 자격증 1급으로 인해 현장 뿐만 아니라 사무 능력까지 겸비할 수 있습니다.'
+'4.팀워크 및 공모전'
+'2023 지역사회 문제해결 소셜번체 해커톤 연합 캠프에서 팀장으로 팀원들과 원활한 커뮤니케이션과 협력을 통해 팀의 목표를 달성하여 대상을 수사안 기록이 있습니다.'
+'5.목표와 포부 '
+'미래에는 소방 기술사 자격증을 딸 계획을 가지고 성장하는 기회를 찾고 있습니다. 현대 중공업에서 제 경험과 능력을 활용하여 함께 싸우고, 도전 목표를 달성하고자 합니다. 저를 뽑아주신다면 지속적으로 싸우고 성장하는 모습을 보여드리겠습니다. 기회를 주셔서 감사합나다.'
+# fig, ax = plt.subplots()
 
-fig, ax = plt.subplots()
+# a = 2
+# b = -5
+# c = 3
+# d = -7
 
-a = 2
-b = -5
-c = 3
-d = -7
+# x = []
+# y = []
 
-x = []
-y = []
-
-for i in range(-100, 101, 50):
-    x.append(i)
-    y.append(a*i**3 + b*i**2 + c*i + d)
-col1, col2, col3 = st.columns(3)
-with col1:
-    color = st.radio('색을 선택하시오.', ('red','green','blue'))
-with col2:
-    linestyle = st.radio('선의 스타일을 선택하시오.', ('-','-.',':'))
-with col3:
-    marker = st.radio('마카의 스타일을 선택하시오.', ('s','^','o'))
+# for i in range(-100, 101, 50):
+#     x.append(i)
+#     y.append(a*i**3 + b*i**2 + c*i + d)
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#     color = st.radio('색을 선택하시오.', ('red','green','blue'))
+# with col2:
+#     linestyle = st.radio('선의 스타일을 선택하시오.', ('-','-.',':'))
+# with col3:
+#     marker = st.radio('마카의 스타일을 선택하시오.', ('s','^','o'))
 # # if 'red' in color:
 # #     t = '-.r^'
 # # if 'green' in color:
@@ -92,13 +102,13 @@ with col3:
 # #     t = '-.b^'
 
 
-plt.plot(x, y, color = color, marker = marker, linestyle = linestyle)
+# plt.plot(x, y, color = color, marker = marker, linestyle = linestyle)
 
-st.pyplot(fig)
+# st.pyplot(fig)
 
-st.write('Hello, *World!* 😎')
+# st.write('Hello, *World!* 😎')
 
-print('Hello, *World!* :sunglasses:')
+# print('Hello, *World!* :sunglasses:')
 # *하나는 기울림
 # **진하게
 # ***기울림 진하게
